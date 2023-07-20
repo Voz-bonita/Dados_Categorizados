7d78183c1d8f1f92e0d22722111c3235bdc28759
Jose Vitor <jose.vitor.sem.c@gmail.com>
2023-07-20 20:21:28 -0300

    Amostra retirada para treino

---
+++ main.r
@@ -0,0 +1,15 @@
+if (!require("pacman")) {
+    install.packages("pacman")
+}
+
+pacman::p_load("readxl", "dplyr", "purrr")
+
+raw_data <- read_excel("dados_trabalho.xlsx") %>%
+    rename_all(~ c("ID", "Idade", "Socioecon", "Casa", "Setor", "Poupanca")) %>%
+    select(-ID)
+
+full_size <- nrow(raw_data)
+n_cols <- ncol(raw_data)
+sample_order <- sample(1:full_size, size = full_size, replace = FALSE)
+train <- raw_data[1:100, ]
+test <- raw_data[101:full_size, ]
