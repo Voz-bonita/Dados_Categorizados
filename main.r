eed10d65a4d53a639438176d867f7ef61f1e9a87
Jose Vitor <jose.vitor.sem.c@gmail.com>
2023-07-21 09:15:14 -0300

    Populando dataframe para analise grafica e tabular

--- main.r
+++ main.r
@@ -31,3 +31,30 @@ all_models <- map(
     )
 ) %>%
     reduce(c)
+
+best_round_threshold <- function(data, model, par) {
+    yhat <- predict(model, data, type = "response") >= par
+    return(sum(data$Poupanca == yhat) / nrow(data))
+}
+
+n_models <- length(all_models)
+model_info_to_plot <- data.frame(matrix(ncol = 4, nrow = n_models)) %>%
+    rename_all(~ c("Parametros", "n_parametros", "AIC", "ACC"))
+for (i in 1:n_models) {
+    current_model <- all_models[[i]]
+    model_info_to_plot[i, "Parametros"] <- current_model$formula
+    model_info_to_plot[i, "n_parametros"] <- length(
+        current_model$coefficients
+    )
+    model_info_to_plot[i, "AIC"] <- current_model$aic
+
+    threshold <- optim(
+        par = 0, fn = best_round_threshold, data = test, model = current_model,
+        method = "Brent", lower = 0, upper = 1
+    )$value
+
+    ytrue <- test$Poupanca
+    yhat <- predict(current_model, test, type = "response") >= threshold
+
+    model_info_to_plot[i, "ACC"] <- sum(ytrue == yhat) / nrow(test)
+}
